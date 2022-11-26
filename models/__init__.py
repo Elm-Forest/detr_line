6e3fbbe2eab2564939ebe58526e16affdb312838
Zhang Jinming <553781523@qq.com>
2022-11-27 02:06:16 +0800

    update custom dataset

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,9 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-from .detr import build
+from .detr import build, build_detr_with_pretrained
 
 
 def build_model(args):
-    return build(args)
+    if args.transfer:
+        return build_detr_with_pretrained(args)
+    else:
+        return build(args)
