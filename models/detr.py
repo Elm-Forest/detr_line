e940d238f063a05dede262ae300dedc2f63a80a7
Elm Forest <timbrightland@gmail.com>
2023-06-25 00:00:26 +0800

    fix bugs

--- models/detr.py
+++ models/detr.py
@@ -380,6 +380,8 @@ def build(args, data_loader):
     matcher = build_matcher(args)
     weight_dict = {'loss_ce': 1, 'loss_bbox': args.bbox_loss_coef}
     weight_dict['loss_giou'] = args.giou_loss_coef
+    if args.line_detection:
+        weight_dict['loss_angles'] = 1
     if args.masks:
         weight_dict["loss_mask"] = args.mask_loss_coef
         weight_dict["loss_dice"] = args.dice_loss_coef
