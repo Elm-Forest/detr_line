85b48fa2519ed92e94b28751e28219df49999f4d
Elm Forest <timbrightland@gmail.com>
2023-06-27 01:53:45 +0800

    fix bugs

--- models/unet/TransUNet.py
+++ models/unet/TransUNet.py
@@ -15,8 +15,9 @@ from util.misc import nested_tensor_from_tensor_list, NestedTensor
 class SegmentationHead(nn.Sequential):
     def __init__(self, in_channels, out_channels, kernel_size=3, upsampling=1):
         conv2d = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=kernel_size // 2)
+        relu = nn.ReLU()
         upsampling = nn.UpsamplingBilinear2d(scale_factor=upsampling) if upsampling > 1 else nn.Identity()
-        super().__init__(conv2d, upsampling)
+        super().__init__(conv2d, relu, upsampling)
 
 
 class Conv2dReLU(nn.Sequential):
