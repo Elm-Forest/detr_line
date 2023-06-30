89419e45da7d07ef9ba1734657e0f1a286b44c3f
Elm Forest <timbrightland@gmail.com>
2023-06-30 17:49:54 +0800

    fix bugs

--- datasets/coco.py
+++ datasets/coco.py
@@ -117,28 +117,42 @@ def make_coco_transforms(image_set):
         T.ToTensor(),
         T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
+    #
+    # # scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
+    # scales = [800]
+    # if image_set == 'train':
+    #     # return T.Compose([
+    #     #     T.RandomHorizontalFlip(),
+    #     #     T.RandomSelect(
+    #     #         T.RandomResize(scales, max_size=1333),
+    #     #         T.Compose([
+    #     #             T.RandomResize([400, 500, 600]),
+    #     #             T.RandomSizeCrop(384, 600),
+    #     #             T.RandomResize(scales, max_size=1333),
+    #     #         ])
+    #     #     ),
+    #     #     normalize,
+    #     # ])
+    #     return T.Compose([
+    #         T.RandomHorizontalFlip(),
+    #         T.RandomResize([512]),  # 将RandomResize的尺度参数设置为[800]
+    #         normalize,
+    #     ])
+    scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
 
-    # scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
-    scales = [800]
     if image_set == 'train':
-        # return T.Compose([
-        #     T.RandomHorizontalFlip(),
-        #     T.RandomSelect(
-        #         T.RandomResize(scales, max_size=1333),
-        #         T.Compose([
-        #             T.RandomResize([400, 500, 600]),
-        #             T.RandomSizeCrop(384, 600),
-        #             T.RandomResize(scales, max_size=1333),
-        #         ])
-        #     ),
-        #     normalize,
-        # ])
         return T.Compose([
             T.RandomHorizontalFlip(),
-            T.RandomResize([512]),  # 将RandomResize的尺度参数设置为[800]
+            T.RandomSelect(
+                T.RandomResize(scales, max_size=1333),
+                T.Compose([
+                    T.RandomResize([400, 500, 600]),
+                    T.RandomSizeCrop(384, 600),
+                    T.RandomResize(scales, max_size=1333),
+                ])
+            ),
             normalize,
         ])
-
     if image_set == 'val':
         return T.Compose([
             T.RandomResize([800], max_size=1333),
